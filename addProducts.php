bba66da0ace953a8352e98f4d19fc76e118e16cc
kirlgildritch <133635795+kirlgildritch@users.noreply.github.com>
2026-04-02 15:53:33 +0800

    shop, addProducts added

---
+++ addProducts.php
@@ -0,0 +1,90 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LocalLift PH - Add Product</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700;800&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="stylesheet" href="assets/css/addProductStyle.css">
+    
+</head>
+<body>
+    <div class="topbar">
+    <div class="container">
+      <div class="topbar-left">
+        <a href="#"><i class="fa-brands fa-facebook-f"></i></a>
+        <a href="#"><i class="fa-brands fa-instagram"></i></a>
+        <a href="#"><i class="fa-brands fa-tiktok"></i></a>
+        <a href="#"><i class="fa-solid fa-envelope"></i></a>
+      </div>
+
+      <div class="topbar-center">
+        <i class="fa-solid fa-truck"></i>
+        <span>FREE SHIPPING THIS WEEK! ORDER OVER ₱500</span>
+      </div>
+
+      <div class="topbar-right">
+        <span>ENGLISH <i class="fa-solid fa-chevron-down"></i></span>
+      </div>
+    </div>
+  </div>
+
+
+  <header class="header">
+    <div class="container header-main">
+      <a href="index.php" class="logo">
+        <div class="logo-icon">
+          <i class="fa-solid fa-location-dot"></i>
+        </div>
+        <div class="logo-text">
+          LocalLift
+          <span>PH</span>
+        </div>
+      </a>
+
+      <form class="search-bar">
+        <input type="text" placeholder="Search for products, shops, and more...">
+        <button type="submit" class="search-btn">
+          <i class="fa-solid fa-magnifying-glass"></i>
+        </button>
+      </form>
+
+      <div class="header-icons">
+        <a href="login.php" class="icon-box">
+          <i class="fa-regular fa-user"></i>
+          Login
+        </a>
+
+        <a href="register.php" class="icon-box">
+          <i class="fa-regular fa-user-plus"></i>
+          Register
+          
+        </a>
+
+    
+      </div>
+    </div>
+  </header>
+
+
+    <nav class="navbar">
+    <div class="container">
+           <div class="container breadcrumb">
+            <a href=""><i class="fa-solid fa-house"></i>&nbsp;Dashboard</a>
+            &nbsp;/&nbsp;
+            <span>Add Product</span>
+    </div>
+
+    </div>
+  </nav>
+
+
+
+
+
+
+</body>
+</html>
